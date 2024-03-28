b2e04d5dc019d4dc75d2f9dda05c1e8870d71031
repeat0105 <kkyzxc40@gmail.com>
2024-03-28 11:42:26 +0900

    Add all files in src directory

---
+++ src/app/page.js
@@ -0,0 +1,42 @@
+import React from "react";
+import "../app/sass/page.scss";
+
+import Image from "next/image";
+
+
+
+import kakao from "/public/kakaolog/kakaotalk_social_media_apps_logo_icon.png";
+import github from "/public/kakaolog/github_icon.png";
+import Link from "next/link";
+import Loginpage from "./(pages)/login/page";
+import UseImagecom from '@/app/component/UseImagecom.jsx'
+import Camera from "./component/Camera";
+
+
+
+
+function apppege(props) {
+  return (
+    <div className="app">
+      <div style={{marginTop: "10px"}}>
+        <UseImagecom />
+        
+        <div>
+          <h2>HEALING LINE</h2>
+          <p>
+            재충전,힐링,휴식 중<br /> 하나만 달성하기
+          </p>
+          {/* <Camera /> */}
+          <Loginpage />
+          <Link href="https://github.com/login" target="_blank">
+            <Image src={github} alt="" />
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default apppege;
+
+

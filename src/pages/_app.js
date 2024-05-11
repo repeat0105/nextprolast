835a286080bed82905ca0f8d6e007e90caf4c607
repeat0105 <kkyzxc40@gmail.com>
2024-05-11 16:17:08 +0900

    vercel

--- src/pages/_app.js
+++
@@ -1,7 +0,0 @@
-import { config } from '@fortawesome/fontawesome-svg-core'
-import '@fortawesome/fontawesome-svg-core/styles.css'
-config.autoAddCss = false
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-};
